42ac8d8bc5ce416de05017bd01f3ac622c970a41
Noza P. <pulotovafaranoz@gmail.com>
2025-12-26 18:32:46 -0500

    I completed the homework.

---
+++ statements.py
@@ -0,0 +1,10 @@
+# Printing numbers
+print(3)
+print(2+5)
+
+# Printing sentences
+print("You can type anything!")
+print("I like drawing.")
+
+# Printing mixed texts and numbers
+print("Only", 5, "more days left till new year!.")
