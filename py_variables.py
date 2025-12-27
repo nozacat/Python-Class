defbe3958e4d234a85f22490bef12d82b746481e
Noza P. <pulotovafaranoz@gmail.com>
2025-12-27 14:13:01 -0500

    Variables topic

---
+++ py_variables.py
@@ -0,0 +1,11 @@
+name = "Noza"   #the type of the variable is a string
+age = 13    #the type of the variable is integer
+favoriteFruits = "kiwi, strawberry, manderin"   #the type is string
+
+print(name)
+print(age)
+print(favoriteFruits)
+
+print(type(name))
+print(type(age))
+print(type(favoriteFruits))
