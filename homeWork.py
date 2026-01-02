0ba9a7c90785e058a0e1a7c625f8bc1d27da1816
Noza P. <pulotovafaranoz@gmail.com>
2026-01-02 16:55:09 -0500

    Finished homework

---
+++ homeWork.py
@@ -0,0 +1,30 @@
+'''
+Create  4 variables:
+
+1st - should be the name of your friend
+2nd - should be the age of your friend
+3rd - should be a short description in two sentace about him
+4th - his favorite subject at school
+
+Print all line by line, and print the type of each variable line by line
+'''
+
+name = "Noishva"
+age = 14
+description = "Noishva is not just a person its 3 people combined. Noishva is really funny and always there for you."
+favoriteSubject = "I think its spanish and lunch"
+
+print(name)
+print(age)
+print(description)
+print(favoriteSubject)
+
+print(type(name))
+print(type(age))
+print(type(description))
+print(type(favoriteSubject))
+
+
+
+
+
